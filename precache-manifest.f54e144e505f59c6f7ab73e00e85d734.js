7091c2d27fa5948a9d72090128098ba5ed3e12d0
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2019-04-09 15:14:04 +0200

    Updates

---
+++ precache-manifest.f54e144e505f59c6f7ab73e00e85d734.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7eca5d54378286c574b",
+    "url": "/trollelo/static/js/runtime~main.bc9131e5.js"
+  },
+  {
+    "revision": "4bebf17669674399a069",
+    "url": "/trollelo/static/js/main.76b9718b.chunk.js"
+  },
+  {
+    "revision": "fc9eca33f536e2b31a62",
+    "url": "/trollelo/static/js/2.cb2a07b3.chunk.js"
+  },
+  {
+    "revision": "4bebf17669674399a069",
+    "url": "/trollelo/static/css/main.78b00de8.chunk.css"
+  },
+  {
+    "revision": "2664ddf8d764a63a5a6cbf03c9806b8f",
+    "url": "/trollelo/index.html"
+  }
+];
